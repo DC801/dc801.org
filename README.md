03045f502c9fa2524ced3a5da70cdb11bd02d9a9
Moggie <moggie@moggie.io>
2016-03-13 03:04:55 -0600

    Updated README and added use of markdown to build.js

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # dc801.org
 Website stuffz
+
+## Things to Keep in Mind
+- This project uses ESLint for JS linting, in accordance with the Airbnb config
--- build.js
+++ build.js
@@ -1,6 +1,8 @@
 const metalsmith = require('metalsmith');
+const markdown = require('metalsmith-markdown');
 
 
 metalsmith(__dirname)
   .destination('./build')
+  .use(markdown())
   .build();
